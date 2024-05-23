33045cf4402a867b1a6c20a49b70aab9d32f6a53
Yashd07 <yashd1272@gmail.com>
2024-05-24 00:06:48 +0530

    update file

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,17 @@ import financialRecordRouter from "./routes/financial-records";
 import cors from "cors";
 
 import dotenv from "dotenv";
+const corsOptions = {
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the methods allowed
+  allowedHeaders: ['Content-Type', 'Authorization'], // Specify the headers allowed
+  optionsSuccessStatus: 200
+};
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 const mongoURI: string = process.env.MONGO_URI || "mongodb+srv://expenso:expenso123@cluster0.ciskzii.mongodb.net/";
 //yash
