e91599a8b419eb92ecd8f9758e3a5cb48348a2d2
Yashd07 <yashd1272@gmail.com>
2024-05-26 20:53:21 +0530

    final changes

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 
 import dotenv from "dotenv";
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://expenso-client-six.vercel.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the methods allowed
   allowedHeaders: ['Content-Type', 'Authorization'], // Specify the headers allowed
   optionsSuccessStatus: 200
