bceee5af2a065753b57b1c57008c346b7ad2f7c1
Yash Desai <104765280+Yashd07@users.noreply.github.com>
2024-07-04 07:43:02 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,18 +11,19 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins or specify your frontend URL
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-});
+
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins or specify your frontend URL
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 
 const mongoURI: string = process.env.MONGO_URI || "mongodb+srv://expenso:expenso123@cluster0.ciskzii.mongodb.net/";
 //yash
